2fd1e84f3e62db814c0ac1362aa31371e9ca70d9
ian2014 <ian9727103@hotmail.com>
2020-07-17 15:04:56 +0800

    Update recognition.py
    Update tessdata_dir_config with customized file

--- 03-edge_computing/03_1-recognition/recognition.py
+++ 03-edge_computing/03_1-recognition/recognition.py
@@ -4,8 +4,12 @@ img = Image.open("1.jpg")
 img = img.convert("L")
 
 #img.show()
-#tessdata_dir_config = "/home/pi/car/tessdata/" 
-#ans = pytesseract.image_to_string(img, lang="chi_tra", config=tessdata_dir_config)
+
+# config path
+#tessdata_dir_config = "sample_data/" 
+
+# lang="FILE_NAME", config="CONFIG_PATH"
+#ans = pytesseract.image_to_string(img, lang="abc", config=tessdata_dir_config)
 
 ans = pytesseract.image_to_string(img, lang="eng")
 print(ans)
